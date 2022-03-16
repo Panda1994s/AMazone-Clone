1a106d2b51a6b4dfd8f87b9d3c903c31d588a686
Vahid Jafari <vahid.bozkurt1994@gmail.com>
2022-03-16 10:44:48 +0100

    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

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCEQ-CY0TewUIh7nBPBuu7kH1xVQYQVq58",
+    authDomain: "clone-abcd1.firebaseapp.com",
+    projectId: "clone-abcd1",
+    storageBucket: "clone-abcd1.appspot.com",
+    messagingSenderId: "911919382335",
+    appId: "1:911919382335:web:4802bd41b6580fded1dd8a",
+    measurementId: "G-B9ZWPDLZB6"
+  };
